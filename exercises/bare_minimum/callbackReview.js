32080d44bb6fd9315b3900aae164296671ef4e4d
John Reiland <reiland.shanghai@gmail.com>
2019-12-20 11:53:28 -0500

    "Commit for OS migration"

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,27 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(filePath, (err, data) => {
+    if (data) {
+      let split = data.toString().split('\n');
+      callback(err, split[0]);
+    } else {
+      callback(err, null);
+    }
+  })
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request(url, function (error, response, body) {
+    if (response) {
+      callback(error, response.statusCode);
+    } else {
+      callback(error, null);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
